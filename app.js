fd7665f88efa17fb48a1b44cc24fa2bf9fb43308
Shurpanchik <Shur1993@mail.ru>
2016-12-06 13:35:34 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
 /*-----------------------------------------------------------------------------
-This Bot demonstrates how to use a waterfall to prompt the user with a series
-of questions.
-
-This example also shows the user of session.userData to persist information
-about a specific user. 
-
-# RUN THE BOT:
-
-    Run the bot from the command line using "node app.js" and then type 
-    "hello" to wake the bot up.
-    
+A simple "Hello World" bot for the Microsoft Bot Framework. 
 -----------------------------------------------------------------------------*/
 
 var restify  = require('restify');
 var builder  = require('botbuilder');
+var route 	 = require('./route.json');
+
+
+//=========================================================
+// Bot Setup
+//=========================================================
 
 // Setup Restify Server
 var server = restify.createServer();
@@ -29,22 +25,164 @@ var connector = new builder.ChatConnector({
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
+var answerMap = new Map();
+
+//=========================================================
+// Functions for set Questions
+//=========================================================
+
+// метода printQuestion() выводит на экран вопрос с вариантами ответов
+function printQuestion(textQuestion,choiseAnswer ){
+return function (session) {
+       builder.Prompts.choice(session,textQuestion , choiseAnswer, { 
+		listStyle: builder.ListStyle.button,
+		retryPrompt : textQuestion
+	   });
+    }
+}
+
+// метод routingAnswer() запоминает ответ на вопрос и перенаправляет к другому вопросу без учета результата предыдущих вопросов
+function routingAnswer (url, choiseAnswerQuestion,choiseRoute, textQuestion ){
+return function (session, results) {
+			if(answerMap.has(url)){answerMap.delete(url);}
+			answerMap.set(url,{
+				text : textQuestion,
+				choiceAnswer : choiseAnswerQuestion,
+				answerUser : choiseAnswerQuestion[results.response.index]
+				});
+			session.beginDialog(choiseRoute[results.response.index]);
+    }	
+}
+
+// setQuestion() задает вопрос и запоминает ответ без учета предыдущих вопросов
+function   setQuestion(url, textQuestion,choiseAnswer,choiseRoute){
+return [printQuestion(textQuestion,choiseAnswer ), routingAnswer (url, choiseAnswer,choiseRoute, textQuestion ),function (session, results) {
+        session.endDialogWithResult()
+    }];
+}
+
+// routingAnswerProxy() запоминает ответ на вопрос и перенаправляет к другому вопросу с учетом результатов предыдущих вопросов
+function routingAnswerProxy (url, choiceAnswerQuestion,urlPrev,choiceAnswerPrev,choiceRoute, textQuestion ){
+return function (session, results) {
+	
+		if(answerMap.has(url)){answerMap.delete(url);}
+		answerMap.set(url,{
+			text : textQuestion,
+			choiceAnswer : choiseAnswerQuestion,
+			answerUser : choiseAnswerQuestion[results.response.index]
+				});
+		session.beginDialog(choiseRoute[results.response.index]);
+	
+		for(var i=0;i<choiceAnswerPrev.length;i++){
+			if(answerMap.get(urlPrev)==choiceAnswerPrev[i]){
+			session.beginDialog(choiceRoute[i]);
+			}
+		}
+    }	
+}
+
+// задает вопрос и запоминает ответ с учетом результатов предыдущих вопросов
+function   setQuestionProxy(url, textQuestion, choiceAnswer,urlPrev,choiceAnswerPrev,choiceRoute){
+return [printQuestion(textQuestion,choiceAnswer ), routingAnswerProxy (url, choiceAnswer,urlPrev,choiceAnswerPrev,choiceRoute, textQuestion ),function (session, results) {
+        session.endDialogWithResult()
+    }];
+}
+// добавляет диалог с вопросом
+function setDialog(q){
+bot.dialog(q.url, 
+  setQuestion(	
+			q.url,
+			q.text,
+			q.choiceAnswer, 
+			q.choiceRouting
+			)		
+);
+}
+// инциализация мапы с информацией о вопросе по url (если надо хранить все вопросы)
+function setAnswerMap(){
+	for(var q in route){
+	answerMap.set(route[q].url,  {
+		text: route[q].text,
+		choiceAnswer :  route[q].choiceAnswer,
+		answerUser : 'На вопрос не отвечали'
+		})
+	}
+}
+//=========================================================
+// Bots Dialogs
+//=========================================================
 bot.dialog('/', [
     function (session) {
-        builder.Prompts.text(session, "Hello... What's your name?");
+        session.beginDialog('/start')
     },
-    function (session, results) {
-        session.userData.name = results.response;
-        builder.Prompts.number(session, "Hi " + results.response + ", How many years have you been coding?"); 
-    },
-    function (session, results) {
-        session.userData.coding = results.response;
-        builder.Prompts.choice(session, "What language do you code Node using?", ["JavaScript", "CoffeeScript", "TypeScript"]);
-    },
-    function (session, results) {
-        session.userData.language = results.response.entity;
-        session.send("Got it... " + session.userData.name + 
-                     " you've been programming for " + session.userData.coding + 
-                     " years and use " + session.userData.language + ".");
-    }
 ]);
+
+bot.dialog('/inprogress', 
+function (session) {
+	var dataResultPoll =''; 
+		if(answerMap.size==0){
+		dataResultPoll='Вы ответили на 0 вопросов';
+		}
+		else{
+			for (var val of answerMap.values()) {
+				dataResultPoll = dataResultPoll+ '\n\n\n\n' + 'На вопрос: '+ val.text + '\n\n'+ ' Вы ответили: ' + val.answerUser  ;	
+			}
+		}
+    session.send(dataResultPoll);
+	answerMap.clear();
+	session.beginDialog('/');
+    }
+);
+bot.dialog('/start', [
+    function (session) {
+		answerMap.clear();
+        session.beginDialog('/ready')
+    },
+]);
+bot.dialog('/menu', [
+    function (session) {
+		answerMap.clear();
+        session.beginDialog('/ready')
+    },
+]);
+
+bot.beginDialogAction('start', '/start', {matches: /^\/?start/i});
+bot.beginDialogAction('menu', '/menu', {matches: /^\/?menu/i});
+
+setDialog(route.ready);
+setDialog(route.AreYouInAnyPain);
+setDialog(route.AreYouInAnyPain_Wo);
+setDialog(route.AreYouInAnyPain_Wo_bottomStomachhow);
+setDialog(route.AreYouInAnyPain_Wo_bottomStomach_how_oftenurine);
+setDialog(route.AreYouInAnyPain_Wo_bottomStomach_howoften_urine_difficulturine);
+setDialog(route.AreYouInAnyPain_Wo_bottomStomach_how_go);
+setDialog(route.AreYouInAnyPain_Wo_bottomStomach_how_oftenurine_difficulturine_all);
+setDialog(route.AreYouInAnyPain_Wo_bottomStomach_how_bloodurine);
+setDialog(route.AreYouInAnyPain_Wo_another_itch);
+setDialog(route.AreYouInAnyPain_Wo_another_burn);
+setDialog(route.AreYouInAnyPain_Wo_another_discomfort);
+setDialog(route.AreYouInAnyPain_Wo_another_penispain);
+setDialog(route.AreYouInAnyPain_Wo_another_penispain_in_excreta);
+setDialog(route.AreYouInAnyPain_Wo_another_penispain_out_openhead);
+setDialog(route.AreYouInAnyPain_Wo_another_penispain_all_erection);
+setDialog(route.AreYouInAnyPain_Wo_another_penispain_in_excreta_color);
+setDialog(route.AreYouInAnyPain_Wo_another_penispain_out_openhead_all);
+setDialog(route.AreYouInAnyPain_Wo_another_penispain_out_openhead_head_red);
+setDialog(route.AreYouInAnyPain_Wo_another_penispain_out_openhead_head_rush);
+setDialog(route.AreYouInAnyPain_Wo_another_penispain_out_openhead_head_peeling);
+setDialog(route.AreYouInAnyPain_Wo_another_penispain_out_openhead_head_warts);
+setDialog(route.AreYouInAnyPain_Wo_another_penispain_out_openhead_head_smell);
+setDialog(route.AreYouInAnyPain_Wo_another_penispain_out_openhead_head_deform);
+setDialog(route.AreYouInAnyPain_Wo_another_penispain_all_erection_deform);
+setDialog(route.DoYouHaveAviolationOfUrination);
+setDialog(route.DoYouHaveAviolationOfUrination_any_oft);
+setDialog(route.DoYouHaveAviolationOfUrination_any_pain);
+setDialog(route.DoYouHaveAviolationOfUrination_any_sluggishstream);
+setDialog(route.DoYouHaveAviolationOfUrination_any_difficulty);
+setDialog(route.DoYouHaveAviolationOfUrination_any_newsnine);
+setDialog(route.YouHaveChangedTheColorOfTheUrine);
+
+
+
+
+
