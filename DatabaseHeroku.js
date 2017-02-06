a75d04f3b2e25852d08dae012b1e3a42c0e2b7b2
Shurpanchik <Shur1993@mail.ru>
2017-02-06 14:50:25 +0400

    Delete DatabaseHeroku.js

--- DatabaseHeroku.js
+++
@@ -1,6 +0,0 @@
-var pg = require('pg');
-  pg.defaults.ssl = true;
-  var answer ='Не подключено';
-const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/todo';
-const client = new pg.Client(connectionString);
-client.connect(); 
