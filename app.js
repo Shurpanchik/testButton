7563432f255bceea9dace4cf30f9f861687f8927
Shurpanchik <Shur1993@mail.ru>
2017-02-07 12:29:45 +0400

    Update app.js

--- app.js
+++ app.js
@@ -207,7 +207,15 @@ bot.dialog('sendMessageToPatient', [
 	function (session, result) {
 		var msg = new builder.Message()
 			.address(JSON.parse(session.userData.addressClient))
-			.text("Вы отправляли сообщение " + session.userData.textquestion + ' Вам ответили: ' + result.response);
+			.text("Вы отправляли сообщение " + session.userData.textquestion + ' Вам ответили: ' + result.response)
+			.attachments([
+				new builder.HeroCard(session)
+					.buttons([builder.CardAction.dialogAction(session, 'sendMessageToPatient', JSON.stringify({
+						"text": result.response,
+						"address": JSON.stringify(session.message.address)
+					}), 'Ответить')])
+			]);
+			
 		bot.send(msg, function (err) {
 			if (err == undefined) {
 				session.send("Ваш ответ успешно доставлен");
